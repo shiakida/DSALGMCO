db35cdace0d5614a4e2a7dce604efe0e26445fd0
Eowyn <eowyn_mangahas@dlsu.edu.ph>
2026-06-05 15:31:38 +0800

    Wyn push

--- stack.c
+++ stack.c
@@ -1,7 +1,6 @@
 /**
 
     >> Encode your group number (2 digits) and last names, first names and sections of group members.  Encode the purpose of this file.
-
      Group #: 12 (2 digits)
 
     MANGAHAS, EOWYN SABRINA  S13
